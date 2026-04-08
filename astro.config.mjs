e2cde973554a9985c8c2a8aa3a00fd97224a2c26
Moran-38B <chococa.minecraft@gmail.com>
2026-04-09 00:17:28 +0800

    chore: rename repo and udpate base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ export default defineConfig({
   // 這裡的 site 請換成你的 GitHub 帳號名
   site: 'https://Moran-38B.github.io',
   // 這裡的 base 必須跟你的 Repo 名稱完全一致，且前後都有斜線
-  base: '/Web.github.io/', 
+  base: '/Web/', 
   integrations: [tailwind()],
 });
